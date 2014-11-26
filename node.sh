ecb5c5859226d404c57618b06faf40f58b15233a
AlexandreBroudin <broudin.alexandre@gmail.com>
2014-11-26 14:01:41 +0100

    Change some packages

--- node.sh
+++ node.sh
@@ -3,4 +3,4 @@
 # Ask for the administrator password upfront
 sudo -v
 
-npm i -g lodash n csslint gulp svgo yo bower less phantomjs grunt-cli grunt-autoprefixer grunt-contrib-less
+npm i -g lodash n csslint gulp svgo yo bower less sass browser-sync phantomjs grunt-cli grunt-contrib-less
